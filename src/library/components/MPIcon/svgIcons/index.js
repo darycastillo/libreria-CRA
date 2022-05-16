bc2d98dc4b006d7dc56790287ebee5a8f9189e04
Dary Castillo <darycastillo27@gmail.com>
2022-05-16 16:14:49 -0600

    MP custom Icons

---
+++ src/library/components/MPIcon/svgIcons/index.js
@@ -0,0 +1,37 @@
+import { ReactComponent as Agregar_plus } from "./agregar_plus.svg";
+import { ReactComponent as AngleDown } from "./angle down.svg";
+import { ReactComponent as AngleUp } from "./angle up.svg";
+import { ReactComponent as ArrowLeft } from "./arrow_left.svg";
+import { ReactComponent as ArrowRight } from "./arrow_right.svg";
+import { ReactComponent as Ayuda } from "./Ayuda.svg";
+import { ReactComponent as Bitacora } from "./bitacora.svg";
+import { ReactComponent as Check } from "./check.svg";
+import { ReactComponent as Editar } from "./editar.svg";
+import { ReactComponent as Eliminar } from "./eliminar.svg";
+import { ReactComponent as ExcelAzul } from "./excelAzul.svg";
+import { ReactComponent as Documento } from "./imprimirSolvencia.svg";
+import { ReactComponent as Search } from "./search.svg";
+import { ReactComponent as CloudUp } from "./cloud_upload.svg";
+import { ReactComponent as Vista } from "./vista.svg";
+import { ReactComponent as VerificarFirma } from "./verificarFirma.svg";
+import { ReactComponent as HuellaFirma } from "./huella_firma.svg";
+
+export {
+  Agregar_plus,
+  AngleDown,
+  AngleUp,
+  ArrowLeft,
+  ArrowRight,
+  Ayuda,
+  Bitacora,
+  Check,
+  Editar,
+  Eliminar,
+  ExcelAzul,
+  Documento,
+  Search,
+  CloudUp,
+  Vista,
+  VerificarFirma,
+  HuellaFirma,
+};
